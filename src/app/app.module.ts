cf873a6b62c0b4100a1d9f72898c4c7cf0a15e16
beta3000 <atlas.aresco@gmail.com>
2018-03-10 10:44:00 -0500

    Tarea terminada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from './success-alert/success-alert.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
